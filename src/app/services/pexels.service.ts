8c30be920876bc6f905951cf86ca04f8db5cf1ae
John <johneliud4@gmail.com>
2026-02-06 21:01:11 +0300

    chore: comment out Pexels service and update gitignore for environment files. This update temporarily disables the Pexels API integration by commenting out the service implementation and broadens the gitignore pattern to exclude all environment files for better secret management.

--- src/app/services/pexels.service.ts
+++ src/app/services/pexels.service.ts
@@ -1,43 +1,43 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { map } from 'rxjs/operators';
+// import { environment } from '../../environments/environment';
 
-interface PexelsPhoto {
-  id: number;
-  src: {
-    medium: string;
-  };
-}
+// interface PexelsPhoto {
+//   id: number;
+//   src: {
+//     medium: string;
+//   };
+// }
 
-interface PexelsResponse {
-  photos: PexelsPhoto[];
-}
+// interface PexelsResponse {
+//   photos: PexelsPhoto[];
+// }
 
-@Injectable({
-  providedIn: 'root'
-})
-export class PexelsService {
-  private apiKey = environment.pexelsApiKey;
-  private baseUrl = 'https://api.pexels.com/v1';
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class PexelsService {
+//   private apiKey = environment.pexelsApiKey;
+//   private baseUrl = 'https://api.pexels.com/v1';
 
-  constructor(private http: HttpClient) {}
+//   constructor(private http: HttpClient) {}
 
-  searchPhotos(query: string, perPage: number = 9): Observable<string[]> {
-    const headers = new HttpHeaders({
-      'Authorization': this.apiKey
-    });
+//   searchPhotos(query: string, perPage: number = 9): Observable<string[]> {
+//     const headers = new HttpHeaders({
+//       'Authorization': this.apiKey
+//     });
 
-    return this.http.get<PexelsResponse>(`${this.baseUrl}/search`, {
-      headers,
-      params: {
-        query,
-        per_page: perPage.toString(),
-        orientation: 'square'
-      }
-    }).pipe(
-      map(response => response.photos.map(photo => photo.src.medium))
-    );
-  }
-}
+//     return this.http.get<PexelsResponse>(`${this.baseUrl}/search`, {
+//       headers,
+//       params: {
+//         query,
+//         per_page: perPage.toString(),
+//         orientation: 'square'
+//       }
+//     }).pipe(
+//       map(response => response.photos.map(photo => photo.src.medium))
+//     );
+//   }
+// }
